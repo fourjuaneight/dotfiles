e9efa580c4cbceb1e858a01ba8414eff2f7ac79c
Juan Villela <juan@villela.co>
2019-03-09 00:50:59 -0500

    Dev and zsh config updates.

--- zsh/.fzf.zsh
+++ zsh/.fzf.zsh
@@ -1,13 +1,14 @@
-# Setup fzf·
-# ---------·
-if [[ ! "$PATH" == */home/fourjuaneight/.fzf/bin* ]]; then·
-  export PATH="$PATH:/home/fourjuaneight/.fzf/bin"·
-fi·
-·
-# Auto-completion·
-# ---------------·
-[[ $- == *i* ]] && source "/home/fourjuaneight/.fzf/shell/completion.zsh" 2> /dev/null·
-·
-# Key bindings·
-# ------------·
-source "/home/fourjuaneight/.fzf/shell/key-bindings.zsh"·
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/fourjuaneight/.fzf/bin* ]]; then
+  export PATH="$PATH:/home/fourjuaneight/.fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/home/fourjuaneight/.fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/home/fourjuaneight/.fzf/shell/key-bindings.zsh"
+
