b59fc1a072b2511d13a31633af99f6dc47b37ab7
Juan Villela <juan@villela.co>
2019-03-22 14:33:17 -0400

    Added pip installs.

---
+++ env/pip.sh
@@ -0,0 +1,11 @@
+python3 -m pip install autopep8
+python3 -m pip install b2
+python3 -m pip install brotli
+python3 -m pip install flake8
+python3 -m pip install fonttools
+python3 -m pip install ipython
+python3 -m pip install proselint
+python3 -m pip install requests
+python3 -m pip install vim-vint
+python3 -m pip install virtualenv
+python3 -m pip install virtualenvwrapper
