a39fbf7cd96e61187b3f0bf7a21a49a401584acb
Juan Villela <juan@villela.co>
2019-10-07 12:34:59 -0400

    Emacs updates
    Moved dashboard widget function to functions file.

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -1,38 +1,5 @@
 (load-theme 'doom-dracula t)
 
-(defun doom-dashboard-widget-banner ()
-  (let ((point (point)))
-    (mapc (lambda (line)
-            (insert (propertize (+doom-dashboard--center +doom-dashboard--width line)
-                                'face 'font-lock-comment-face) " ")
-            (insert "\n"))
-          '("      @@@  @@@  @@@   @@@@@@   @@@  @@@ "
-            "      @@@  @@@  @@@  @@@@@@@@  @@@@ @@@ "
-            "      @@!  @@!  @@@  @@!  @@@  @@!@!@@@ "
-            "      !@!  !@!  @!@  !@!  @!@  !@!!@!@! "
-            "      !!@  @!@  !@!  @!@!@!@!  @!@ !!@! "
-            "      !!!  !@!  !!!  !!!@!!!!  !@!  !!! "
-            "      !!:  !!:  !!!  !!:  !!!  !!:  !!! "
-            " !!:  :!:  :!:  !:!  :!:  !:!  :!:  !:! "
-            " ::: : ::  ::::: ::  ::   :::   ::   :: "
-            "  : :::     : :  :    :   : :  ::    :  "))
-    (when (and (display-graphic-p)
-               (stringp fancy-splash-image)
-               (file-readable-p fancy-splash-image))
-      (let ((image (create-image (fancy-splash-image-file))))
-        (add-text-properties
-         point (point) `(display ,image rear-nonsticky (display)))
-        (save-excursion
-          (goto-char point)
-          (insert (make-string
-                   (truncate
-                    (max 0 (+ 1 (/ (- +doom-dashboard--width
-                                      (car (image-size image nil)))
-                                   2))))
-                   ? ))))
-      (insert (make-string (or (cdr +doom-dashboard-banner-padding) 0)
-                           ?\n)))))
-
 (setq
  css-indent-offset 2
  doom-big-font (font-spec :family "Dank Mono" :size 36)
@@ -81,3 +48,4 @@
 (move-text-default-bindings)
 
 (load! "+bindings")
+(load! "+functions")
