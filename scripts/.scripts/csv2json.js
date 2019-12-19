e9b50c5232ea031556b94096a24dfbf5feadea19
Juan Villela <juan@villela.co>
2019-12-18 22:17:27 -0500

    Added CSV to JSON converter script.

---
+++ scripts/.scripts/csv2json.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+const { readdir, writeFileSync } = require('fs');
+const { join } = require('path');
+const csv = require('csvtojson');
+
+const home = __dirname.match(/\/Users\/[a-z]+/g);
+const source = join(home[0], 'csv2json');
+
+readdir(source, (err, files) => {
+  if (err) {
+    throw err;
+  }
+  files.map(src => {
+    const json = src.replace(/csv/g, 'json');
+
+    csv()
+      .fromFile(`${source}/${src}`)
+      .then(newFile =>
+        writeFileSync(
+          join(source, json),
+          JSON.stringify(newFile, undefined, 2),
+          'utf8'
+        )
+      );
+  });
+});
