7e0d32a883d577d1f4f65d789e077f03d3e4c0e0
Juan Villela <juan@villela.co>
2020-06-25 10:54:23 -0400

    Added git/fzf git commit browser zsh function.

--- zsh/zfunc.zsh
+++ zsh/zfunc.zsh
@@ -315,6 +315,19 @@ grh() {
   git reset --hard $(echo "$commit" | sed "s/ .*//")
 }
 
+# fshow - git commit browser
+gshow() {
+  git log --graph --color=always \
+      --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
+  fzf --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort \
+      --bind "ctrl-m:execute:
+                (grep -o '[a-f0-9]\{7\}' | head -1 |
+                xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
+                {}
+FZF-EOF"
+}
+
+
 # Emacs Diff
 ediff() {
   emacs --eval "(ediff-files \"$1\" \"$2\")";
