ee6f84e8a932bd3032950757cf1c6ec073703f49
Juan Villela <juan@villela.co>
2020-06-25 22:11:17 -0400

    Added missing macOS dependencies and apps.

--- macos/brewCask.sh
+++ macos/brewCask.sh
@@ -1,13 +1,27 @@
 # Brew Cask Packages
 brew cask install alfred \
+  arq-cloud-backup \
+  backblaze \
   bartender \
+  choosy \
   emacs \
   firefox \
+  gitup \
   google-chrome \
+  handbrake \
   hazel \
+  imageoptim \
   iterm2 \
+  moom \
+  openemu \
+  screens-connect \
+  slack \
+  sketch \
+  skype \
+  tower \
   transmit \
   upwork \
-  visual-studio-code
+  visual-studio-code \
+  zoomus
 
 brew cleanup
--- macos/mas.sh
+++ macos/mas.sh
@@ -5,11 +5,21 @@ echo "Sign into the Mac App Store"
 read -s -p "Enter Password: " pswd
 mas signin juan@villela.co "$pswd"
 
-mas install824183456
-mas install1091189122
-mas install1026349850
-mas install1055273043
-mas install1176895641
-mas install904280696
-mas install1107421413
-mas install1333542190
+mas install 1107421413 # 1Blocker
+mas install 1333542190 # 1Password
+mas install 824171161 # Affinity Designer
+mas install 411643860 # DaisyDisk
+mas install 924726344 # Deliveries
+mas install 1435957248 # Drafts
+mas install 1493996622 # Front and Center
+mas install 1081413713 # GIF Brewery
+mas install 1474335294 # GoodLinks
+mas install 1468691718 # Jayson
+mas install 1457450145 # Octotree
+mas install 403504866 # PCalc
+mas install 1055273043 # PDF Expert
+mas install 1503136033 # Service Station
+mas install 1176895641 # Spark
+mas install 425424353 # The Unarchiver
+mas install 904280696 # Things
+mas install 1289378661 # Twitterrific
