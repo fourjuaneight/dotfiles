c129d1a60310d546f182da16c58aa4f108be889b
Juan Villela <juan@villela.co>
2020-12-15 15:16:15 -0500

    Added helper git config scripts.

---
+++ scripts/createGitSigningKey.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+signingkey=$(git config --global --get user.signingkey)
+
+if [[ ! -z $signingkey ]] && [[ ${#signingkey} == 16 ]] # signingkey is not empty and exactly 16 characters long
+then
+echo "Your Git user.signingkey is: "$signingkey
+else
+echo "No global user.signingkey configured for Git. Let's create it."
+echo 
+echo "Use RSA, Size 4096"
+    if  [[ -z $(gpg --list-secret-keys) ]] # no keys exist
+    then
+        gpg --full-generate-key
+    fi
+    gpg --list-secret-keys --keyid-format LONG
+    read -p "Key |sec| rsa4096/*key* [SC]: " signingkey
+    git config --global user.signingkey "$signingkey"
+    gpg --armor --export $signingkey
+    echo "Add the public key above to your Github Accout."
+    ./CreateGitSigningKey.sh
+    history -c
+fi
---
+++ scripts/createGithubSSHKey.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [[ -f ~/.ssh/id_rsa_github ]]
+then
+    echo "Your SSH Key id_rsa_github for Github already exists."
+else
+    echo "Your SSH Key id_rsa_github for Github doesn't exist. Let's create it."
+
+    read -p "Use Git user.email [y/n]: " email
+    if [[ $email == "y" ]] 
+    then
+        email=$(git config --global --get user.email)
+    else
+        read -p "Your Email: " email
+    fi
+    ssh-keygen -t rsa -b 4096 -C "$email" -f ~/.ssh/id_rsa_github
+    echo
+    cat ~/.ssh/id_rsa_github.pub
+    echo
+    echo "Add this Key to your Github Account before you continue."
+    echo
+    history -c
+fi
