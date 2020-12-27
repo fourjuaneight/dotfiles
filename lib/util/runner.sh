4ca596319e91c43d25ab567327a4f4c23b5b2fbc
Juan Villela <juan@villela.co>
2020-12-27 00:12:24 -0500

    Added sudo getter function.

--- lib/util/runner.sh
+++ lib/util/runner.sh
@@ -10,3 +10,18 @@ function run() {
     exit 2
   fi
 }
+
+function getSudo() {
+  running "getting sudo password"
+  sudo -v &> /dev/null
+
+  # Update existing `sudo` time stamp
+  # until this script has finished.
+  #
+  # https://gist.github.com/cowboy/3118588
+
+  # Keep-alive: update existing `sudo` time stamp until script has finished
+  while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+  ok "Password cached"
+}
