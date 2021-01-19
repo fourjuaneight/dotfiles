d24a69336428931e6e3022903574751cc3977040
Juan Villela <juan@villela.co>
2021-01-19 15:37:11 -0500

    Updated npm dependencies.

--- lib/npm.sh
+++ lib/npm.sh
@@ -5,17 +5,17 @@ source ${HOME}/dotfiles/lib/util/echos.sh
 minibot "Little Gary here! Let's download the universe."
 
 action "installing global npm dependencies"
-npm i -g @babel/cli \
+npm i -g @angular/cli\
+  @babel/cli \
+  @nestjs/cli \
+  apollo \
   eslint \
+  fkill-cli \
   gatsby-cli \
   glyphhanger \
-  graphqurl \
-  graphql-tools \
-  gulp-cli \
   ngrok \
   npm-fzf \
   prettier \
-  simple-node-logger \
   typescript \
   webpack-cli
 
