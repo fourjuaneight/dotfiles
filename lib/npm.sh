a710e070b0324db52794875f2e78e5a4755a16ae
Juan Villela <juan@villela.co>
2021-01-27 10:31:40 -0500

    Updated npm dependencies.

--- lib/npm.sh
+++ lib/npm.sh
@@ -7,14 +7,26 @@ minibot "Little Gary here! Let's download the universe."
 action "installing global npm dependencies"
 npm i -g @angular/cli\
   @babel/cli \
+  @capacitor/cli \
   @nestjs/cli \
+  @typescript-eslint/eslint-plugin \
+  @typescript-eslint/parser \
   apollo \
   eslint \
+  eslint-config-airbnb \
+  eslint-config-prettier \
+  eslint-plugin-html \
+  eslint-plugin-import \
+  eslint-plugin-jsx-a11y \
+  eslint-plugin-prettier \
+  eslint-plugin-react \
+  eslint-plugin-react-hooks \
   fkill-cli \
   gatsby-cli \
   glyphhanger \
   ngrok \
   npm-fzf \
+  postcss \
   prettier \
   typescript \
   webpack-cli
