530a767295020161f8f7b6dc2ffa9cc3487ea063
Juan Villela <juan@villela.co>
2021-02-03 17:42:43 -0500

    Added fzf zsh completion and bindings.

--- homedir/.zsh/fzf.zsh
+++ homedir/.zsh/fzf.zsh
@@ -4,7 +4,7 @@ if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
 fi
 
 # Auto-completion
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "~/.zsh/completions/completion.zsh" 2> /dev/null
 
 # Key bindings
-source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+source "~/.zsh/bindings/key-bindings.zsh"
