14271aa95b70c1439e4d2964fa2e7896b98e0256
Juan Villela <juan@villela.co>
2021-07-07 14:54:01 -0400

    Removed deprecated install steps.

--- install.sh
+++ install.sh
@@ -29,22 +29,6 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 fi
 
 # ###########################################################
-# /etc/hosts -- spyware/ad blocking
-# ###########################################################
-read -r -p "Overwrite /etc/hosts with the ad-blocking hosts file from someonewhocares.org? (from ./configs/hosts file) [y|N] " response
-if [[ $response =~ (yes|y|Y) ]]; then
-  action "cp /etc/hosts /etc/hosts.backup"
-  sudo cp /etc/hosts /etc/hosts.backup
-  ok
-  action "cp ./configs/hosts /etc/hosts"
-  sudo cp ./configs/hosts /etc/hosts
-  ok
-  bot "Your /etc/hosts file has been updated. Last version is saved in /etc/hosts.backup"
-else
-  ok "skipped hosts update."
-fi
-
-# ###########################################################
 # Install development tooling and enviroments
 # ###########################################################
 bot "Installing development tooling."
@@ -218,6 +202,10 @@ fi
 # ###########################################################
 bot "Ok. Let's wrap things up."
 
+action "installing zsh plugings"
+sheldon lock
+if [[ $? != 0 ]]; then
+  error "unable to run sheldon lock"
   exit 2
 else
   ok "zsh plugins installed."
@@ -234,15 +222,6 @@ fi
 
 action "Setting Github CLI editor"
 gh config set editor vim
-
-read -r -p "run nightly cron updates? [y|N] " response
-if [[ $response =~ (y|yes|Y) ]]; then
-  action "adding nightly cron updates"
-  cron ${HOME}/.crontab
-  ok "updates croned."
-else
-  ok "skipped croning updates."
-fi
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
   action "setting macOS defaults"
