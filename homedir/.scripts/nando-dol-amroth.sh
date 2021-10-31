ce3c71b249f45a3d3cc1879430a87c8cc61dfd27
Juan Villela <fourjuaneight@users.noreply.github.com>
2021-10-31 02:28:52 -0400

    Added additional directories to S3 sync script.

--- homedir/.scripts/nando-dol-amroth.sh
+++ homedir/.scripts/nando-dol-amroth.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+rclone copy --log-level INFO --ignore-existing  ~/Books s3:dol-amroth/Books
+rclone copy --log-level INFO --ignore-existing  ~/Comics s3:dol-amroth/Comics
 rclone copy --log-level INFO --ignore-existing  ~/Documents s3:dol-amroth/Documents
+rclone copy --log-level INFO --ignore-existing  ~/Manga s3:dol-amroth/Manga
 rclone copy --log-level INFO --ignore-existing  ~/Picture/Wallpapers s3:dol-amroth/Documents/Wallpapers
+rclone copy --log-level INFO --ignore-existing  ~/Plex/Audiobooks s3:dol-amroth/Audiobooks
+rclone copy --log-level INFO --ignore-existing  ~/Plex/Music s3:dol-amroth/Music
+rclone copy --log-level INFO --ignore-existing  ~/ROMs s3:dol-amroth/ROMs
