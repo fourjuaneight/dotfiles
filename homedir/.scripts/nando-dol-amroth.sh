13a0d4dbd8a3e9ff6c00eb64d983cb8dd348e381
Juan Villela <juan@villela.co>
2021-11-01 12:29:43 -0400

    Added log file to S3 sync script.

--- homedir/.scripts/nando-dol-amroth.sh
+++ homedir/.scripts/nando-dol-amroth.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-rclone copy --log-level INFO --ignore-existing  ~/Books s3:dol-amroth/Books
-rclone copy --log-level INFO --ignore-existing  ~/Comics s3:dol-amroth/Comics
-rclone copy --log-level INFO --ignore-existing  ~/Documents s3:dol-amroth/Documents
-rclone copy --log-level INFO --ignore-existing  ~/Manga s3:dol-amroth/Manga
-rclone copy --log-level INFO --ignore-existing  ~/Picture/Wallpapers s3:dol-amroth/Documents/Wallpapers
-rclone copy --log-level INFO --ignore-existing  ~/Plex/Audiobooks s3:dol-amroth/Audiobooks
-rclone copy --log-level INFO --ignore-existing  ~/Plex/Music s3:dol-amroth/Music
-rclone copy --log-level INFO --ignore-existing  ~/ROMs s3:dol-amroth/ROMs
+touch ~/nando-dol-amroth.log || exit
+
+rclone copy --log-level INFO --ignore-existing --log-file ~/nando-dol-amroth.log ~/Books s3:dol-amroth/Books
+rclone copy --log-level INFO --ignore-existing --log-file ~/nando-dol-amroth.log ~/Comics s3:dol-amroth/Comics
+rclone copy --log-level INFO --ignore-existing --log-file ~/nando-dol-amroth.log ~/Documents s3:dol-amroth/Documents
+rclone copy --log-level INFO --ignore-existing --log-file ~/nando-dol-amroth.log ~/Manga s3:dol-amroth/Manga
+rclone copy --log-level INFO --ignore-existing --log-file ~/nando-dol-amroth.log ~/Picture/Wallpapers s3:dol-amroth/Documents/Wallpapers
+rclone copy --log-level INFO --ignore-existing --log-file ~/nando-dol-amroth.log ~/Plex/Audiobooks s3:dol-amroth/Audiobooks
+rclone copy --log-level INFO --ignore-existing --log-file ~/nando-dol-amroth.log ~/Plex/Music s3:dol-amroth/Music
+rclone copy --log-level INFO --ignore-existing --log-file ~/nando-dol-amroth.log ~/ROMs s3:dol-amroth/ROMs
