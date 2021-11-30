228a86bb421251afd722595b726f97d40a5a13c4
Juan Villela <juan@villela.co>
2021-11-30 13:38:12 -0500

    Added npm dependency.

--- lib/npm.sh
+++ lib/npm.sh
@@ -5,6 +5,6 @@ source ./lib/util/echos.sh
 minibot "Little Gary here! Let's download the universe."
 
 action "installing global npm dependencies"
-npm i -g eslint fkill-cli glyphhanger prettier
+npm i -g eslint fkill-cli glyphhanger prettier serve
 
 ok "dome taking up half our storage."
