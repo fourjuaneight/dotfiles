ded73f0b89387d12af96b94619cea9a9baabc8d1
Juan Villela <juan@villela.co>
2022-01-06 13:10:35 -0500

    Updated unzip script.

--- homedir/.scripts/unzip.sh
+++ homedir/.scripts/unzip.sh
@@ -6,17 +6,9 @@ cd ~/Downloads
 
 for file in ~/Downloads/*; do
   case $file in
-    *.tar.bz2)  xcompress x $file ~/Downloads  ;;
-    *.tar.gz)   xcompress x $file ~/Downloads  ;;
-    *.bz2)      xcompress x $file ~/Downloads  ;;
-    *.rar)      xcompress x $file ~/Downloads  ;;
-    *.gz)       xcompress x $file ~/Downloads  ;;
-    *.tar)      xcompress x $file ~/Downloads  ;;
-    *.tbz2)     xcompress x $file ~/Downloads  ;;
-    *.tgz)      xcompress x $file ~/Downloads  ;;
-    *.zip)      xcompress x $file ~/Downloads  ;;
-    *.Z)        xcompress x $file ~/Downloads  ;;
-    *.7z)       xcompress x $file ~/Downloads  ;;
-    *)          echo "Skipping '$file'."       ;;
+    *.tar.gz) xcompress x $file ~/Downloads ;;
+    *.tar)    xcompress x $file ~/Downloads ;;
+    *.zip)    xcompress x $file ~/Downloads ;;
+    *)        echo "Skipping '$file'."      ;;
   esac
 done
