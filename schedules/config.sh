fc8ce89403f94bcad330719252f062c63dc3fd0d
Juan Villela <juan@villela.co>
2022-02-06 01:48:29 +0000

    Added systemd config script.

---
+++ schedules/config.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+source ~/dotfiles/lib/util/echos.sh
+
+action "setting up scheduled tasks"
+
+chmod +x ~/.scripts/*.sh
+if [[ "$OSTYPE" == "darwin"* ]]; then
+else
+  sudo cp ~/dotfiles/schedules/systemd/* /etc/systemd/user/
+
+  action "enable services"
+  for file in ./schedules/systemd/*.service; do
+    systemctl --user start "${file}"
+    systemctl --user enable "${file}"
+  done
+
+  action "enable timers"
+  for file in ./schedules/systemd/*.timer; do
+    systemctl --user start "${file}"
+    systemctl --user enable "${file}"
+  done
+
+  action "reloading daemon"
+  systemctl --user daemon-reload
+fi
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  action "adding systemctl util"
+  git clone https://github.com/joehillen/sysz.git
+  cd sysz
+  sudo make install # /usr/local/bin/sysz
+fi
