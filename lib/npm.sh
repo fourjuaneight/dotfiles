9f9a50e270885ec605fbdf6158c83965b1cdab80
Juan Villela <juan@villela.co>
2022-02-06 02:11:55 +0000

    Minor syntax fixes.

--- lib/npm.sh
+++ lib/npm.sh
@@ -4,7 +4,7 @@ source ~/dotfiles/lib/util/echos.sh
 
 minibot "Let's download the universe."
 eval "$(fnm env)"
-source ~/.bashrc
+
 ~/.cargo/bin/fnm install --lts
 ~/.cargo/bin/fnm use $(~/.cargo/bin/fnm list | ~/.cargo/bin/sd "\*\s" "" | ~/.cargo/bin/sd "\n" "" | ~/.cargo/bin/sd "%" "" | ~/.cargo/bin/sd ".*v(\d+\.\d+\.\d+)\slts-.*" '$1')
 
