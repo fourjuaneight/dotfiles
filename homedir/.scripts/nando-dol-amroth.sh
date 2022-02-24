7de42daee76fd1d285d19dce3c23035d07551e77
Juan Villela <juan@villela.co>
2022-02-23 19:43:32 -0500

    Updated media paths for backup scripts.

--- homedir/.scripts/nando-dol-amroth.sh
+++ homedir/.scripts/nando-dol-amroth.sh
@@ -2,12 +2,12 @@
 
 touch ~/nando-dol-amroth.log || exit
 
-rclone copy --log-level INFO --ignore-existing --log-file ~/nando-dol-amroth.log ~/Books s3:dol-amroth/Books
-rclone copy --log-level INFO --ignore-existing --log-file ~/nando-dol-amroth.log ~/Comics s3:dol-amroth/Comics
+rclone copy --log-level INFO --ignore-existing --log-file ~/nando-dol-amroth.log /mnt/Books s3:dol-amroth/Books
+rclone copy --log-level INFO --ignore-existing --log-file ~/nando-dol-amroth.log /mnt/Comics s3:dol-amroth/Comics
 rclone copy --log-level INFO --ignore-existing --log-file ~/nando-dol-amroth.log ~/Documents s3:dol-amroth/Documents
-rclone copy --log-level INFO --ignore-existing --log-file ~/nando-dol-amroth.log ~/Manga s3:dol-amroth/Manga
+rclone copy --log-level INFO --ignore-existing --log-file ~/nando-dol-amroth.log /mnt/Manga s3:dol-amroth/Manga
 rclone copy --log-level INFO --ignore-existing --log-file ~/nando-dol-amroth.log ~/Picture/Wallpapers s3:dol-amroth/Documents/Wallpapers
-rclone copy --log-level INFO --ignore-existing --log-file ~/nando-dol-amroth.log ~/Plex/Audiobooks s3:dol-amroth/Audiobooks
-rclone copy --log-level INFO --ignore-existing --log-file ~/nando-dol-amroth.log ~/Plex/Music s3:dol-amroth/Music
+rclone copy --log-level INFO --ignore-existing --log-file ~/nando-dol-amroth.log /mnt/Audiobooks s3:dol-amroth/Audiobooks
+rclone copy --log-level INFO --ignore-existing --log-file ~/nando-dol-amroth.log /mnt/Music s3:dol-amroth/Music
 rclone copy --log-level INFO --ignore-existing --log-file ~/nando-dol-amroth.log ~/ROMs s3:dol-amroth/ROMs
 rclone copy --log-level INFO --ignore-existing --log-file ~/nando-dol-amroth.log /mnt/Anime s3:dol-amroth/Anime
