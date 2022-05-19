ddeca6915d3542c9beb24fa0fdbfba60a821c1f1
Juan Villela <juan@villela.co>
2022-05-18 21:28:30 -0400

    Added movies and tv shows to b2 backup.

--- homedir/.scripts/nando-dol-amroth.sh
+++ homedir/.scripts/nando-dol-amroth.sh
@@ -2,13 +2,15 @@
 
 touch ~/nando-dol-amroth.log || exit
 
+rclone copy --log-level INFO --ignore-existing --log-file ~/nando-dol-amroth.log /mnt/Anime b2:helms-deep/Media/Anime
+rclone copy --log-level INFO --ignore-existing --log-file ~/nando-dol-amroth.log /mnt/Audiobooks b2:helms-deep/Media/Audiobooks
 rclone copy --log-level INFO --ignore-existing --log-file ~/nando-dol-amroth.log /mnt/Books b2:helms-deep/Media/Books
 rclone copy --log-level INFO --ignore-existing --log-file ~/nando-dol-amroth.log /mnt/Comics b2:helms-deep/Media/Comics
-rclone copy --log-level INFO --ignore-existing --log-file ~/nando-dol-amroth.log ~/Documents b2:helms-deep/Media/Documents
 rclone copy --log-level INFO --ignore-existing --log-file ~/nando-dol-amroth.log /mnt/Manga b2:helms-deep/Media/Manga
-rclone copy --log-level INFO --ignore-existing --log-file ~/nando-dol-amroth.log ~/Pictures/Wallpapers b2:helms-deep/Media/Documents/Wallpapers
-rclone copy --log-level INFO --ignore-existing --log-file ~/nando-dol-amroth.log ~/Pictures/Library b2:helms-deep/Media/Photos
-rclone copy --log-level INFO --ignore-existing --log-file ~/nando-dol-amroth.log /mnt/Audiobooks b2:helms-deep/Media/Audiobooks
+rclone copy --log-level INFO --ignore-existing --log-file ~/nando-dol-amroth.log /mnt/Movies b2:helms-deep/Media/Movies
 rclone copy --log-level INFO --ignore-existing --log-file ~/nando-dol-amroth.log /mnt/Music b2:helms-deep/Media/Music
+rclone copy --log-level INFO --ignore-existing --log-file ~/nando-dol-amroth.log /mnt/TV_Shows b2:helms-deep/Media/TV_Shows
+rclone copy --log-level INFO --ignore-existing --log-file ~/nando-dol-amroth.log ~/Documents b2:helms-deep/Media/Documents
+rclone copy --log-level INFO --ignore-existing --log-file ~/nando-dol-amroth.log ~/Pictures/Library b2:helms-deep/Media/Photos
+rclone copy --log-level INFO --ignore-existing --log-file ~/nando-dol-amroth.log ~/Pictures/Wallpapers b2:helms-deep/Media/Documents/Wallpapers
 rclone copy --log-level INFO --ignore-existing --log-file ~/nando-dol-amroth.log ~/ROMs b2:helms-deep/Media/ROMs
-rclone copy --log-level INFO --ignore-existing --log-file ~/nando-dol-amroth.log /mnt/Anime b2:helms-deep/Media/Anime
