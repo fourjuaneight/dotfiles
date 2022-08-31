324026f1ea4340ef1db54404faeca9cf139bf6de
Juan Villela <juan@villela.co>
2022-08-31 12:03:50 -0400

    Added copilot to nvim config.

--- homedir/.config/nvim/lua/user/plugins.lua
+++ homedir/.config/nvim/lua/user/plugins.lua
@@ -93,6 +93,9 @@ return packer.startup(function(use)
 	-- Git
 	use({ "lewis6991/gitsigns.nvim", commit = "c18e016864c92ecf9775abea1baaa161c28082c3" })
 
+	-- Copilot
+	use({ "github/copilot.vim" })
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
