6c52078eccb3a1c37e988d5e22f86ef99c1455a8
Juan Villela <juan@villela.co>
2022-09-02 17:43:48 -0400

    Added nvim word highlight toggle.

--- homedir/.config/nvim/lua/user/whichkey.lua
+++ homedir/.config/nvim/lua/user/whichkey.lua
@@ -88,6 +88,7 @@ local mappings = {
   ["w"] = { "<cmd>w!<CR>", "Save" },
   ["q"] = { "<cmd>q!<CR>", "Quit" },
   ["c"] = { "<cmd>Bdelete!<CR>", "Close Buffer" },
+  ["H"] = { "<cmd>:set hlsearch<CR>", "Highlight" },
   ["h"] = { "<cmd>nohlsearch<CR>", "No Highlight" },
   ["f"] = {
     "<cmd>lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown{previewer = false})<cr>",
