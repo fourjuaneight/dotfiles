f73c56ac3113ed81df025cc11747962bd686d3ce
Juan Villela <juan@villela.co>
2023-01-05 13:26:41 -0500

    Minor syntax updates.

--- lib/pip.sh
+++ lib/pip.sh
@@ -5,9 +5,9 @@ source ~/dotfiles/lib/util/echos.sh
 minibot "Ok. Let's try to install some pips."
 
 action "installing python3 packages"
-python3 -m pip install --user b2 brotli fonttools openai psutil pynvim setuptools_rust torch torchvision zopfli Pygments
+python3 -m pip install --user b2 brotli fonttools openai psutil pynvim setuptools_rust zopfli Pygments
 
 action "installing stable diffusion related packages"
-python3 -m pip install accelerate diffusers ftfy invisible-watermark transformers==4.19.2
+python3 -m pip install accelerate diffusers ftfy invisible-watermark torch torchvision transformers==4.19.2
 
 ok "oh hey, it worked! done installing pips."
