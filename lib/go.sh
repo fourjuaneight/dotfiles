e9ab6db5d37a24c8c3bdc57efeba8904191ba343
Juan Villela <juan@villela.co>
2023-02-19 01:46:41 -0500

    Added go setup script.

---
+++ lib/go.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+minibot "Let's GO."
+
+wget https://golang.org/dl/go1.19.1.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf go1.19.1.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin >> ~/.profile
+source ~/.profile
+
+action "Install Golang Language Server"
+go install github.com/nametake/golangci-lint-langserver@latest
+go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.42.1
+go install github.com/cweill/gotests/gotests
+go install github.com/fatih/gomodifytags
+go install github.com/josharian/impl
+go install github.com/haya14busa/goplay/cmd/goplay
+go install github.com/go-delve/delve/cmd/dlv
+go install honnef.co/go/tools/cmd/staticcheck
+go install golang.org/x/tools/gopls
--- lib/wrap-up.sh
+++ lib/wrap-up.sh
@@ -67,17 +67,6 @@ else
   ok "theme saved."
 fi
 
-action "Install Golang Language Server"
-go install github.com/nametake/golangci-lint-langserver@latest
-go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.42.1
-go install github.com/cweill/gotests/gotests
-go install github.com/fatih/gomodifytags
-go install github.com/josharian/impl
-go install github.com/haya14busa/goplay/cmd/goplay
-go install github.com/go-delve/delve/cmd/dlv
-go install honnef.co/go/tools/cmd/staticcheck
-go install golang.org/x/tools/gopls
-
 action "Install Tenjin"
 cd ~
 git clone git@github.com:fourjuaneight/tenjin.git
