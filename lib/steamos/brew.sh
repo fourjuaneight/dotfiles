a4bcb683e7a37cd5f8ef10c5a318b403891d0672
Juan Villela <juan@villela.co>
2023-02-21 14:48:53 -0500

    Added homebrew install for steamos.

---
+++ lib/steamos/brew.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+source ~/dotfiles/lib/util/echos.sh
+
+bot "Installing Homebrew."
+
+running "checking homebrew..."
+brew_bin=$(which brew) 2>&1 >/dev/null
+if [[ $? != 0 ]]; then
+  action "installing homebrew"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  if [[ $? != 0 ]]; then
+    error "unable to install homebrew"
+    exit 2
+  else
+    ok "homebrew installed."
+  fi
+else
+  ok
+fi
+
+minibot "Let's get some beers."
+eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+
+action "turning off analytics"
+brew analytics off
+
+action "updating homebrew directories"
+brew update
+
+action "installing complicated packages"
+brew install homebrew-ffmpeg/ffmpeg/ffmpeg --HEAD
+brew tap helix-editor/helix
+brew install helix
+
+action "installing packages"
+brew install ack \
+  fzf \
+  gh \
+  gum \
+  pnpm \
+  rclone \
+  webp \
+  yt-dlp/taps/yt-dlp
+
+action "cleaning up"
+brew cleanup
+
+ok "we are now proper drunk."
