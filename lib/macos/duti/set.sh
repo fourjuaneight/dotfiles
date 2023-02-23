0936fef124dde57294a975947b7470b60caf8ff2
Juan Villela <juan@villela.co>
2023-02-23 12:03:19 -0500

    Updated macos defaults.

--- lib/macos/duti/set.sh
+++ lib/macos/duti/set.sh
@@ -14,7 +14,24 @@ while read -r ext; do
     exit 2
   fi
 done <"${DUTI_DIR}/media.txt"
-ok "app for media files set."
+
+action "setting default app for vector image files"
+while read -r ext; do
+  /opt/homebrew/bin/duti -s com.seriflabs.affinitydesigner "$ext" all
+  if [[ $? != 0 ]]; then
+    error "unable to set app for $ext files"
+    exit 2
+  fi
+done <"${DUTI_DIR}/vector.txt"
+
+action "setting default app for raster image files"
+while read -r ext; do
+  /opt/homebrew/bin/duti -s com.seriflabs.affinityphoto "$ext" all
+  if [[ $? != 0 ]]; then
+    error "unable to set app for $ext files"
+    exit 2
+  fi
+done <"${DUTI_DIR}/raster.txt"
 
 action "setting default app for code files"
 while read -r ext; do
@@ -24,6 +41,8 @@ while read -r ext; do
     exit 2
   fi
 done <"${DUTI_DIR}/dev.txt"
-ok "app for code files set."
+
+action "setting default app for PDF files"
+/opt/homebrew/bin/duti -s com.readdle.PDFExpert-Mac "$ext" all
 
 ok "done setting default apps."
