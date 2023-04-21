4c7649a84dbf92dbf43348d257ee2e2946f89e6d
Juan Villela <juan@villela.co>
2023-04-21 13:50:45 -0400

    Updated node and python install scripts.

---
+++ lib/node.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+source ~/dotfiles/lib/util/echos.sh
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  softwareupdate --install-rosetta
+fi
+
+minibot "Let's download the universe."
+eval "$(~/.cargo/bin/fnm env)"
+
+~/.cargo/bin/fnm install 16
+~/.cargo/bin/fnm use 16
+
+action "installing global npm dependencies"
+pnpm i -g @angular/cli eslint fkill-cli glyphhanger next prettier serve tailwindcss typescript
+
+ok "done taking up half our storage."
