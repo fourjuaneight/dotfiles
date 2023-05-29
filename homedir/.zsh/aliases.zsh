dea278958b32951c698729320d9f3b510cc57d8d
Juan Villela <juan@villela.co>
2023-05-29 12:49:35 -0400

    Added alias for sudo rm.

--- homedir/.zsh/aliases.zsh
+++ homedir/.zsh/aliases.zsh
@@ -20,6 +20,7 @@ alias hiccup="czkawka_cli"
 alias dupefs="czkawka_cli"
 alias plexperm="sudo chown -R plex.plex"
 alias pueue="pueue -c ~/.config/pueue/pueue.yml"
+alias srip="sudo ~/.cargo/bin/rip -d"
 
 # alternative to 'cat'
 alias cat="bat"
