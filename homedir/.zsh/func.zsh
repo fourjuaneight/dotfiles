be5fb283ef7ccad78e6b7c07220d620d2a137a3b
Juan Villela <juan@villela.co>
2023-06-02 12:48:50 -0400

    Minor syntax updates.

--- homedir/.zsh/func.zsh
+++ homedir/.zsh/func.zsh
@@ -3,7 +3,7 @@ zsh-defer source ${HOME}/dotfiles/lib/util/echos.sh
 # UTILITIES #
 
 # choose from different zellij layouts
-zjl() {
+zlj() {
   local action
   action=$(gum choose "dev" "git") &&
 
