953d9071c6d761ce3be9b1e13ec44b5db3bab274
Juan Villela <13276705+fourjuaneight@users.noreply.github.com>
2024-05-16 17:01:39 -0400

    Minor syntax cleanup.

--- homedir/.zsh/func.zsh
+++ homedir/.zsh/func.zsh
@@ -784,6 +784,22 @@ gecm() {
   git rebase -i "$id^"
 }
 
+# git select commit and list all modified files since
+glmf() {
+  local commits commit id modFiles cleanFiles
+  commits=$(git log --color --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --reverse) &&
+  commit=$(echo "$commits" | fzf --ansi --tac --no-sort --exact) &&
+  id=$(echo "$commit" | sd " .*" "")
+  modFiles=$(git log --pretty=format:"" --name-only --abbrev-commit $id..HEAD) &&
+  cleanFiles=$(echo "$modFiles" | sed '/^$/d' | sort -u) &&
+
+  if [[ -n $cleanFiles ]]; then
+    echo "$cleanFiles"
+  else
+    echo "No modified files found!"
+  fi
+}
+
 # GITHUB CLI #
 
 # git create new PR, add title, select reviewer
@@ -817,22 +833,6 @@ gmpr() {
     selectedPR=$(gh pr list | fzf --ansi --tac --no-sort --exact) &&
       PR=$(echo $selectedPR | sd '^([0-9]+).*' '$1') &&
       gh pr merge $PR -s -d
-  fi
-}
-
-# git select commit and list all modified files since
-glmf() {
-  local commits commit id modFiles cleanFiles
-  commits=$(git log --color --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --reverse) &&
-  commit=$(echo "$commits" | fzf --ansi --tac --no-sort --exact) &&
-  id=$(echo "$commit" | sd " .*" "")
-  modFiles=$(git log --pretty=format:"" --name-only --abbrev-commit $id..HEAD) &&
-  cleanFiles=$(echo "$modFiles" | sed '/^$/d' | sort -u) &&
-
-  if [[ -n $cleanFiles ]]; then
-    echo "$cleanFiles"
-  else
-    echo "No modified files found!"
   fi
 }
 
