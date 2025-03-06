e8aeb8ce2e0bb66d41ed5a33682c159538b75453
Juan Villela <13276705+fourjuaneight@users.noreply.github.com>
2025-03-06 13:03:12 -0500

    Removed ai python packages.

--- lib/python.sh
+++ lib/python.sh
@@ -7,7 +7,4 @@ minibot "Ok. Let's try to install some pips."
 action "installing python3 packages"
 python3 -m pip install --user --break-system-packages b2 brotli fonttools openai psutil pynvim setuptools_rust zopfli Pygments
 
-action "installing stable diffusion related packages"
-python3 -m pip install accelerate diffusers ftfy invisible-watermark torch torchvision transformers==4.19.2
-
 ok "oh hey, it worked! done installing pips."
