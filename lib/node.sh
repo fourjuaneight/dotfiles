2c04c8246ea2c7596413a3bde2ac0e90c2a0d7e3
Juan Villela <13276705+fourjuaneight@users.noreply.github.com>
2025-03-06 13:10:43 -0500

    Update node version for global packages.

--- lib/node.sh
+++ lib/node.sh
@@ -9,8 +9,8 @@ fi
 minibot "Let's download the universe."
 eval "$(~/.cargo/bin/fnm env)"
 
-~/.cargo/bin/fnm install 18
-~/.cargo/bin/fnm use 18
+~/.cargo/bin/fnm install 20.18.3
+~/.cargo/bin/fnm use 20.18.3
 
 action "installing global npm dependencies"
 export PUPPETEER_SKIP_DOWNLOAD='true';
