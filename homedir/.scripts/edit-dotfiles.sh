4e7d25d802633fb0cccfc05eba0fb6724b907621
Juan Villela <fourjuaneight@users.noreply.github.com>
2025-07-15 16:55:38 -0400

    Added raycast script to open dotfiles in vscode.

---
+++ homedir/.scripts/edit-dotfiles.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Edit Dotfiles
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.author fourjuaneight
+# @raycast.authorURL https://raycast.com/fourjuaneight
+
+code ~/dotfiles
