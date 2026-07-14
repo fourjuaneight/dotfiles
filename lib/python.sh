5735af3f245ccbaa438c404d82ea5ce4f52fc3f6
Juan Villela <fourjuaneight@users.noreply.github.com>
2026-07-14 14:50:01 -0400

    Added linux brew python installs.

--- lib/python.sh
+++ lib/python.sh
@@ -7,6 +7,8 @@ minibot "Ok. Let's try to install some pips."
 action "installing python3 packages"
 if [[ "$OSTYPE" == "darwin"* ]]; then
   python3 -m pip install --user b2 brotli fonttools openai psutil pynvim setuptools_rust zopfli Pygments
+elif command -v /home/linuxbrew/.linuxbrew/bin/python3 &>/dev/null; then
+  /home/linuxbrew/.linuxbrew/bin/python3 -m pip install b2 brotli fonttools openai psutil pynvim setuptools_rust zopfli Pygments
 else
   python3 -m pip install --user --break-system-packages b2 brotli fonttools openai psutil pynvim setuptools_rust zopfli Pygments
 fi
