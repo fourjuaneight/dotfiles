ea81c894a2119b32192bf67776368d74941fda46
Juan Villela <fourjuaneight@users.noreply.github.com>
2026-07-14 23:35:44 -0400

    Add tailscale download to mac.

--- lib/macos/apps.sh
+++ lib/macos/apps.sh
@@ -6,7 +6,7 @@ minibot "Let's install some apps."
 
 cd ~/Downloads
 
-action "downloading UpWork"
-curl -O https://upwork-usw2-desktopapp.upwork.com/binaries/v5_8_0_24_aef0dc8c37cf46a8/Upwork.dmg
+action "installing Tailscale"
+curl -O https://pkgs.tailscale.com/stable/Tailscale-latest-macos.pkg
 
 ok "done installing apps."
